af061ba2660dc82d3a3e19ef7183a6f92eb0db0f
rajahir <raj.chandravadiya@etatvasoft.com>
2024-02-06 18:48:03 +0530

    Add project files.

---
+++ Controllers/login.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDocMVC.Controllers
+{
+    public class login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult login_page()
+        {
+            return View();
+        }
+        public IActionResult forgot_password()
+        {
+            return View();
+        }
+    }
+}
