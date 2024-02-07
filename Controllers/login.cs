4a86ad7ccdea2d4107b9c9728488fb28c3a2c1e4
rajahir <raj.chandravadiya@etatvasoft.com>
2024-02-07 18:32:15 +0530

    3 tier architecture

--- Controllers/login.cs
+++ Controllers/login.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HalloDocDAL.DataContext;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HalloDocMVC.Controllers
 {
     public class login : Controller
     {
+        private readonly ApplicationDbContext _context;
         public IActionResult Index()
         {
             return View();
